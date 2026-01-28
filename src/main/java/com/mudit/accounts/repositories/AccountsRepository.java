189b85b2e294c67bd32855ebff5e3e04c34ef965
Mudit Khanna <muditkhanna998@gmail.com>
2026-01-27 23:45:08 -0500

    Implement account deletion logic and API endpoint
    - Add `deleteAccountDetails` DELETE endpoint to `AccountsController`
    - Implement `deleteAccount` in `AccountsServiceImpl` to remove customer and account data
    - Add `deleteAccount` method signature to `IAccountsService`
    - Add `deleteByCustomerId` to `AccountsRepository` with `@Modifying` and `@Transactional` annotations

--- src/main/java/com/mudit/accounts/repositories/AccountsRepository.java
+++ src/main/java/com/mudit/accounts/repositories/AccountsRepository.java
@@ -2,10 +2,16 @@ package com.mudit.accounts.repositories;
 
 import com.mudit.accounts.entities.Accounts;
 import com.mudit.accounts.entities.Customer;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 
 import java.util.Optional;
 
 public interface AccountsRepository extends JpaRepository<Accounts, Long> {
     Optional<Accounts> findByCustomerId(Long customerId);
+
+    @Transactional
+    @Modifying
+    void deleteByCustomerId(Long customerId);
 }
