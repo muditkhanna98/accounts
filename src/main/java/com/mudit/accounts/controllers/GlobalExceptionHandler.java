db9af348ecc9ae0fe2840277474d5b827223eafe
Mudit Khanna <muditkhanna998@gmail.com>
2026-01-28 00:00:41 -0500

    Implement validation exception handling in GlobalExceptionHandler
    - Extend `ResponseEntityExceptionHandler` to support standard Spring MVC exceptions
    - Add `handleMethodArgumentNotValid` method to process `MethodArgumentNotValidException`
    - Return a map of field-specific validation errors with `BAD_REQUEST` status

--- src/main/java/com/mudit/accounts/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/mudit/accounts/controllers/GlobalExceptionHandler.java
@@ -3,16 +3,26 @@ package com.mudit.accounts.controllers;
 import com.mudit.accounts.dtos.ErrorResponseDto;
 import com.mudit.accounts.exceptions.CustomerAlreadyExistsException;
 import com.mudit.accounts.exceptions.ResourceNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(CustomerAlreadyExistsException.class)
     public ResponseEntity<ErrorResponseDto> handleCustomerAlreadyExists
             (CustomerAlreadyExistsException exception, WebRequest webRequest) {
@@ -47,5 +57,19 @@ public class GlobalExceptionHandler {
                 LocalDateTime.now()
         );
         return new ResponseEntity<>(errorResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> validationErrors = new HashMap<>();
+        List<ObjectError> validationErrorList = ex.getBindingResult().getAllErrors();
+
+        validationErrorList.forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String validationMsg = error.getDefaultMessage();
+            validationErrors.put(fieldName, validationMsg);
+        });
+        return new ResponseEntity<>(validationErrors, HttpStatus.BAD_REQUEST);
     }
 }
